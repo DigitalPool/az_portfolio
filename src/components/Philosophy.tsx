76e4d873871fa43e9c6d3d0d053a245fb8e76263
abu_qaanitah <mp@abuqaanitahmac.local>
2025-04-24 23:11:56 +0100

    update quotes

--- src/components/Philosophy.tsx
+++ src/components/Philosophy.tsx
@@ -8,10 +8,10 @@ export default function Philosophy() {
 				</p>
 
 				<h2 className="text-7xl font-extrabold leading-tight text-[#b7aa98]">
-				&ldquo;We don&apos;t have to wait for the
+				We dont have to wait for the
 					<span className="text-red-500"> most perfect condition </span>
 					else we get
-					<span className="text-red-500"> nothing done.</span>&rdquo;
+					<span className="text-red-500"> nothing done.</span>
 				</h2>
 
 				<p className="uppercase tracking-[0.3em] text-sm text-bold text-neutral-500 mb-12">

72cb54c48218352dd385ff7ba18d49e59e182942
abu_qaanitah <mp@abuqaanitahmac.local>
2025-04-24 21:02:01 +0100

    return to dev state

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+// next.config.ts
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	output: 'export',
+	basePath: '/az_portfolio',
+	assetPrefix: '/az_portfolio/',
+	images: {
+		unoptimized: true,
+	},
+	// eslint: {
+	// 	ignoreDuringBuilds: true,
+	// },
+
 };
 
 export default nextConfig;
